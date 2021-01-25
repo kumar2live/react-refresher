126a5ffee6a423818f7430cd0e322a1cf7e91abe
Muthu Kumar <kumar.bsc@live.com>
2021-01-25 23:21:02 +0800

    webpack-config

---
+++ test-lib/src/App.js
@@ -0,0 +1,15 @@
+
+import {ShowYourNameComponent} from 'show-your-name'
+import 'show-your-name/dist/index.css'
+
+function App() {
+  return (
+    <div >
+      Hello React
+
+      <ShowYourNameComponent text="MK"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ using-webpack/test-one/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <p>Hello there! it's working</p>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
