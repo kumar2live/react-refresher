201d7798cbdea82d9611020522e973affb26c276
Muthu Kumar <kumar.bsc@live.com>
2021-01-26 00:01:43 +0800

    update

--- using-webpack/test-one/src/index.tsx
+++ using-webpack/test-one/src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { FC } from "react";
 import ReactDOM from "react-dom";
 // import Test from "./app/test";
 
 import image from "./assets/react.png";
 import "./styles.scss";
 
-const App = () => {
+const App: FC = () => {
   return (
     <>
-      <p>Hello there! it's working with different path running in - {process.env.NODE_ENV}</p>
+      <p>Hello there! it's working with different path running in - {process.env.NODE_ENV} with TS</p>
 
       <img src={image} height={200} width={400} />
 
