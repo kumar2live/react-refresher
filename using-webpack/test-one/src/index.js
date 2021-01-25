f91fd03fd87e1f75eb7238e0c54e87031ad9e953
Muthu Kumar <kumar.bsc@live.com>
2021-01-25 23:55:04 +0800

    update

--- using-webpack/test-one/src/index.js
+++ using-webpack/test-one/src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import Test from "./app/test";
 
+import image from "./assets/react.png";
+import "./styles.scss";
+
 const App = () => {
   return (
     <>
-      <p>Hello there! it's working with different path</p>
+      <p>Hello there! it's working with different path running in - {process.env.NODE_ENV}</p>
+
+      <img src={image} height={200} width={400} />
 
       {/* <Test /> */}
     </>
--- using-webpack/test-one/webpack.config.js
+++ using-webpack/test-one/webpack.config.js
@@ -1,9 +1,11 @@
 const HtmlPlugin = require("html-webpack-plugin");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
   module: {
@@ -25,12 +27,30 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(s[ac]ss)$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
     new HtmlPlugin({
       filename: "index.html",
       template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
     }),
   ],
   devServer: {
